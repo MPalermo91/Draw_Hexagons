3552fce1ebdcc6a79c56b48c3d0c7416522a1e07
Maximiliano Palermo <maxi.palermo.racing@gmail.com>
2022-04-02 17:18:57 -0300

    V3_02-03
    Juego semi terminado.
    Solo quedaria por resolver el tema de la pantalla de derrota para que dure mas tiempo en lugar de volver al menu principal

--- Assets/Script/MovPlayer.cs
+++ Assets/Script/MovPlayer.cs
@@ -8,37 +8,47 @@ public class MovPlayer : MonoBehaviour
 {
     public float speed = 0.05f;
     public float contPuntos = 0;
-    public float contadorTiempo;
-    public bool destruiObjeto;
+
+    public int contadorTiempo;
     private int vida = 1;
+
+    public bool destruiObjeto;
 
     public GameObject condicionDerrota;
     public GameObject tiempoRegresoMenu;
+    public GameObject cantPuntosObtenidos;
 
     public Text txtPuntos;
     public Text txtTiempo;
+    public Text txtPuntajeFinal;
 
     public AudioSource soundPunto;
     public AudioSource soundMorir;
 
     public void Start()
     {
-        contadorTiempo = 5000;
+        contadorTiempo = 0;
 
         txtPuntos.GetComponent<Text>();
-        txtPuntos.text = contPuntos.ToString("00");
         txtTiempo.GetComponent<Text>();
-        txtTiempo.text = contadorTiempo.ToString("00");
         
         condicionDerrota.SetActive(false);
         tiempoRegresoMenu.SetActive(false);
+        cantPuntosObtenidos.SetActive(false);
+
         soundMorir.Pause();
         soundPunto.Pause();
     }
     void Update()
     {
-       
-        
+        txtPuntajeFinal.text = "Has Obtenido: " + cantPuntosObtenidos.ToString() + " Puntos";
+        txtTiempo.text = contadorTiempo.ToString();
+        txtPuntos.text = "Score: " + contPuntos.ToString();
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
         if (Input.GetKey(KeyCode.A))
         {
             this.transform.Translate(-speed * Time.deltaTime, 0, 0);
@@ -58,20 +68,24 @@ public class MovPlayer : MonoBehaviour
 
         if (vida <= 0)
         {
-            // Destroy(this.gameObject);   //Destruye el objeto que colisiona con este objeto (menos los trigger)
-            this.gameObject.SetActive(false);
+            Destroy(this.gameObject);   //Destruye el objeto que colisiona con este objeto (menos los trigger)
+            //this.gameObject.SetActive(false);
         }
-        for (float t = 0; destruiObjeto == true && t >= 5; t++)
+        for (int t = 0; destruiObjeto == true; t++)
         {
-            contadorTiempo--;
-            Debug.Log("Contador de tiempo: " + contadorTiempo);
-            
-            if (contadorTiempo <= 0)
+            //PuedeContarTiempo(true);
+            contadorTiempo++;
+            condicionDerrota.SetActive(true);
+            tiempoRegresoMenu.SetActive(true);
+            cantPuntosObtenidos.SetActive(true);
+
+            if (contadorTiempo >= 50)
             {
                 destruiObjeto = false;
                 SceneManager.LoadScene("MainMenu");
             }
         }
+       
 
     }
 
@@ -80,14 +94,11 @@ public class MovPlayer : MonoBehaviour
         vida--;
         soundMorir.Play();
         destruiObjeto = true;
-        condicionDerrota.SetActive(true);
-        tiempoRegresoMenu.SetActive(true);
     }
 
     private void OnTriggerEnter2D(Collider2D collision) // Para colisiones con objetos trigger (son transpasables pero colisionan)
     {
         contPuntos ++; // Suma puntos cada vez que atravieza el collider "Trigger" de un Hexagono sin chocar con sus lados visibles.
-        Debug.Log("Score: "+ contPuntos);
         soundPunto.Play();
     }
 
